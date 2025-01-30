dc4a182ad88eddfc42f0fedbb6b85af5c857930b
cristianpalmer <cpalmer@colgate.edu>
2025-01-30 15:01:14 -0500

    Finished Task 1 Step 2

--- Working R.R
+++ Working R.R
@@ -1,7 +1,13 @@
-library(stringr)
-music_dirs <- list.dirs("MUSIC", recursive = TRUE)
-print(music_dirs)
-str_count(music_dirs, "/")
+library("stringr")
+
+
+
+usic_dirs <- list.dirs("MUSIC", recursive = TRUE)
+
+slash_counts <- str_count(music_dirs, "/")
+
+music_dirs [which(slash_counts == 2)]
+
 
 
 
