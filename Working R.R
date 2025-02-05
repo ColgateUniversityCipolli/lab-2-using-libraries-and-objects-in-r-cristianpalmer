659632bf27cb7d915265946a9a703fe9260cad94
cristianpalmer <cpalmer@colgate.edu>
2025-02-04 21:16:44 -0500

    Finished task 1

--- Working R.R
+++ Working R.R
@@ -5,25 +5,25 @@ library("stringr")
 music_dirs <- list.dirs("MUSIC", recursive = TRUE)
 
 ################ Task 1 Step 2 
+code_to_process <- c()
 slash_counts <- str_count(music_dirs, "/")
 
 album_dirs <- music_dirs [which(slash_counts == 2)]
-
+for (i in 1:length(album_dirs)) {
 
 ################ Task 1 Step 3 Part 1
 # For each album one at a time
-curr.album <- album_dirs[1]
+curr.album <- album_dirs[i]
 files_in_album <- list.files(curr.album)
 
 
 ################ Task 1 Step 3 Part 2
 WAV_counts <- str_count(files_in_album, ".wav")
 files_in_album <- files_in_album [which(WAV_counts == 1)]
-
+for (j in 1:length(files_in_album)) {
 
 ################ Task 1 Step 3 Part 3 Part A
-code_to_process = vector(mode = "character", length = 0)
-track = files_in_album[1]
+track = files_in_album[j]
 Track_Title <- paste('"',curr.album, "/",track,'"', sep = "")
 Track_Title
 
@@ -44,3 +44,13 @@ album_name <- split_albumname[length(split_albumname)]
 
 Desired.Output.File <- paste(artist_name, "-",album_name,'-',track_name,'.',"json", sep = "")
 Desired.Output.File
+
+################ Task 1 Step 3 Part 3 Part C
+Final.output <- paste('streaming_extractor_music.exe "', track, '" "', Desired.Output.File, '"', sep = "")
+code_to_process <- c(code_to_process, Final.output)
+}
+}
+################ Step 4
+writeLines(code_to_process, "batfile.txt")
+
+
